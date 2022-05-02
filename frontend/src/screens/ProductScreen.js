835a63849baeb86e6dfc6fc27d99eda5258eb88e
Harry-Yates <hyates1@gmail.com>
2022-05-02 20:12:12 +0200

    Set up product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
 const ProductScreen = () => {
-    return <div>ProductScreen</div>;
+    const { id } = useParams();
+    // const product = products.find(product => product._id === Number(id));
+    const product = products[0];
+
+    return (
+        <>
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup varient='flush'>
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
+                            />
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </>
+    );
 };
 
 export default ProductScreen;
